6dd086aa76225982f6d84b5b97fe66ca836c3b94
Andrew Ho <andrewho@andrewho.co.uk>
2012-11-07 02:23:18 +0000

    Separate out remind alias

--- remind/aliases.zsh
+++ remind/aliases.zsh
@@ -1,7 +1,8 @@
 # Print out a fullscreen calendar
-
 pretty_remind () {
     clear
     remind -c+$(( ((`tput lines` - 5) / 8) + 1 )) -g -m -b1 -w`tput cols` $* | head -n $(( `tput lines` - 2 ))
     echo
 }
+
+alias rem=$'pretty_remind ~/.remind/remind'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,2 @@
 alias ls='ls -lp'
-alias rem=$'pretty_remind ~/.remind/remind'
 [[ $(uname -s) == 'Darwin' ]] && alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
