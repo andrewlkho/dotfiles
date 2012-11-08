8c16f6db81d93c43b444e54cc1434e05031c29e4
Andrew Ho <andrewho@andrewho.co.uk>
2012-11-08 02:54:08 +0000

    Make sure pip uses /usr/local

--- python/options.zsh
+++ python/options.zsh
@@ -1,2 +1,3 @@
-# Augment the default search path for modules
+# Augment the default search path for modules.  Don't forget to add this to sudoers:
+#   Defaults env_keep += "PYTHONPATH"
 export PYTHONPATH=$( find /usr/local/lib -type d -name 'site-packages' )
