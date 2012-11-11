aac6ac6e64d98cb6356fe70f1f4fc46d2c058faf
Andrew Ho <andrewho@andrewho.co.uk>
2012-11-11 20:51:19 +0000

    Default options for ack

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias ls='ls -lp'
+alias ack='ack-grep --all --no-color --smart-case --sort-files --pager="less -x4SF"'
 [[ $(uname -s) == 'Darwin' ]] && alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
