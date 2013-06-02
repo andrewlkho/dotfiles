770ec440d5e7a0424df8c95d86376285d44761aa
Andrew Ho <andrewho@andrewho.co.uk>
2013-06-02 21:03:20 +0100

    Hard-code python library path
    Debian wheezy also contains the directory
    /usr/local/lib/python2.6/site-packages.  The easiest way to avoid confusion
    is to hard-code the 2.7 path into PYTHONPATH.

--- python/options.zsh
+++ python/options.zsh
@@ -1,3 +1,3 @@
 # Augment the default search path for modules.  Don't forget to add this to sudoers:
 #   Defaults env_keep += "PYTHONPATH"
-export PYTHONPATH=$( find /usr/local/lib -type d -name 'site-packages' )
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages
