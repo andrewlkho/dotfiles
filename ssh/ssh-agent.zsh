0eed343e8eae2340317761b37b064a8bc21185fe
Andrew Ho <andrewho@andrewho.co.uk>
2013-10-02 14:53:07 +0100

    Update ssh-agent setup to use gpg-agent

--- ssh/ssh-agent.zsh
+++ ssh/ssh-agent.zsh
@@ -1,13 +1,24 @@
-# Setup ssh-agent and propagate across sessions via ~/.zshrc-ssh
-if [[ -a $HOME/.ssh/id_rsa ]]; then
-    [[ -a $HOME/.zshrc-ssh ]] && . $HOME/.zshrc-ssh
-    if [[ ! -S $SSH_AUTH_SOCK && ! (`uname -s` == 'Darwin' && `uname -r | cut -d . -f 1` -ge 9) ]]; then
-        local EXPORT_SSH
-        killall ssh-agent >& /dev/null
-        EXPORT_SSH=$( ssh-agent | awk -F '; ' '/SSH/ { print $1 }' | sed -e 'N; s/\n/ /' -e 's/^/export /' )
-        echo $EXPORT_SSH > $HOME/.zshrc-ssh
-        eval $EXPORT_SSH
-        unset EXPORT_SSH
-        ssh-add $HOME/.ssh/id_rsa*
+# If I am logged on to a console on a Mac then this is a local machine not 
+# server so setup gpg-agent and propagate across sessions via ~/.zshrc-ssh
+if [[ ! -n $SSH_TTY && `uname -s` == "Darwin" ]]; then
+    if [[ -a $HOME/.zshrc-ssh ]]; then
+        . $HOME/.zshrc-ssh
+        export GPG_AGENT_INFO SSH_AUTH_SOCK SSH_AGENT_PID
+    fi
+    kill -0 $SSH_AGENT_PID &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        eval $( gpg-agent \
+            --daemon \
+            --enable-ssh-support \
+            --write-env-file=$HOME/.zshrc-ssh )
+    fi
+fi
+
+# If this is my server (the only machine I forward an SSH agent to) then make
+# sure the socket is correctly setup with a link in ~/.ssh-agent-sock
+if [[ `hostname -s` == "sebastian" && -n $SSH_AUTH_SOCK ]]; then
+    if [[ $SSH_AUTH_SOCK == /tmp/* ]]; then
+        ln -sf $SSH_AUTH_SOCK $HOME/.ssh-agent-sock
+        export SSH_AUTH_SOCK=$HOME/.ssh-agent-sock
     fi
 fi
