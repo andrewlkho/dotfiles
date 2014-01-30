0822841b5b597c251092d6354cc8d1a38a97f67f
Andrew Ho <andrewho@andrewho.co.uk>
2014-01-30 15:10:09 +0000

    ST3 is in a slightly different location

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias ls='ls -lp'
 alias ack='ack-grep --all --no-color --smart-case --sort-files --pager="less -x4SF"'
 [[ $(uname -s) == 'Darwin' ]] && alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
-[[ $(uname -s) == 'Darwin' ]] && alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+[[ $(uname -s) == 'Darwin' ]] && alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
