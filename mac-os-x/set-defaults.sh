fb16a9005fb2a061943bcfc625f020c8b8e448ae
Andrew Ho <andrewho@andrewho.co.uk>
2014-07-26 19:37:56 +0100

    Expand print dialogue in OS X

--- mac-os-x/set-defaults.sh
+++ mac-os-x/set-defaults.sh
@@ -1,9 +1,12 @@
 # I don't use dashboard
-defaults write com.apple.dashboard mcx-disabled -boolean YES
+defaults write com.apple.dashboard mcx-disabled -bool true
 killall Dock
 
 # Show ~/Library
 chflags nohidden ~/Library
 
 # The save dialogue should always open in expanded view
-defaults write -g NSNavPanelExpandedStateForSaveMode -bool TRUE
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Expand print dialogue by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
