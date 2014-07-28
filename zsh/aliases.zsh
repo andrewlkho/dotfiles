84662b24629565d33cfd76d4fa5f2ab2725e9849
Andrew Ho <andrewho@andrewho.co.uk>
2014-07-28 23:43:19 +0100

    Alias for mergepdf

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,7 @@
 alias ls='ls -lp'
 alias ack='ack-grep --all --no-color --smart-case --sort-files --pager="less -x4SF"'
+
+# OS X specific
 [[ $(uname -s) == 'Darwin' ]] && alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
-[[ $(uname -s) == 'Darwin' ]] && alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+# Usage: mergepdf -o output.pdf 1.pdf 2.pdf...
+[[ $(uname -s) == 'Darwin' ]] && alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
