7fe72e8f5e5ed94e85b2ce1cc9023ffb6fd191af
Andrew Ho <andrewho@andrewho.co.uk>
2015-04-23 19:14:12 +0100

    Refactor zsh prompt
    Inspiration from:
    https://github.com/sindresorhus/pure

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,30 +1,57 @@
 setopt PROMPT_SUBST
 
-git_repo() {
-    echo $( pwd | awk 'match($0, /[^\/]+\.git/) { print substr($0, RSTART, RLENGTH-4) }' )
-}
-
-git_branch() {
-    echo $( /usr/bin/git symbolic-ref HEAD | cut -d'/' -f 3 )
-}
-
-is_dirty() {
-    [[ -n $( /usr/bin/git status -s ) ]] && echo "*"
-}
-
-is_unpushed() {
-    [[ -n $( /usr/bin/git cherry -v @{upstream} 2>/dev/null ) ]] && echo "↑"
-}
-
-git_prompt() {
-    # This works because I name all of my git directories *.git (and is faster
-    # than parsing git status)
-    if [[ $PWD =~ "\.git" ]]; then
-        echo $(git_repo)" → "$(git_branch)$(is_unpushed)$(is_dirty)" | "
+# print the current directory, highlighting the git top level directory
+_prompt_pwd() {
+    local pwd=$( print -Pn "%~" )
+    if [[ $( git rev-parse --is-inside-work-tree 2> /dev/null ) == "true" ]]; then
+        local git_tld=$( basename $( git rev-parse --show-toplevel ) )
+        print -Pn "%F{blue}${pwd/${git_tld}/"%F{166}${git_tld}%F{blue}"}%f"
     else
-        echo ""
+        print -Pn "%F{blue}${pwd}%f"
     fi
 }
 
-export PS1='[%m] $(git_prompt)%1~ %# '
-export PS2='[%m] $(git_prompt)%1~ > '
+# print info about the current git branch
+_prompt_git_info() {
+    if [[ $( git rev-parse --is-inside-work-tree 2> /dev/null ) == "true" ]]; then
+        echo -n " ⟶  "
+        echo -n "$( git symbolic-ref HEAD | cut -d'/' -f 3 )"
+        [[ -n $( git status --porcelain --ignore-submodules ) ]] && print -Pn "%F{red}*%f"
+        (( $( git rev-list --left-only --count HEAD...@'{u}' ) > 0 )) && print -Pn " %F{cyan}⇡%f"
+    fi
+}
+
+# print exec time of previous command if > 5s
+_prompt_cmd_exec_time() {
+    elapsed=$(( ${EPOCHSECONDS} - ${cmd_starttime:-${EPOCHSECONDS}} ))
+    if (( ${elapsed} > 5 )); then
+        echo -n " "
+        (( ${elapsed} / 86400 > 0 )) && echo -n "$(( ${elapsed} / 86400 ))d "
+        (( ${elapsed} / 3600 % 24 )) && echo -n "$(( ${elapsed} / 3600 % 24 ))h "
+        (( ${elapsed} / 60 % 60 )) && echo -n "$(( ${elapsed} / 60 % 60 ))m "
+        echo "$(( ${elapsed} % 60 ))s"
+    fi
+}
+
+# preexec is executed just after a command has been read and is about to be executed
+_prompt_preexec() {
+    cmd_starttime=${EPOCHSECONDS}
+}
+
+# precmd is executed before each prompt
+_prompt_precmd() {
+    print -P "\n`_prompt_pwd``_prompt_git_info` %n@%m%F{yellow}`_prompt_cmd_exec_time`%f"
+    unset cmd_starttime
+}
+
+_prompt_setup() {
+    zmodload zsh/datetime
+    autoload -U add-zsh-hook
+
+    add-zsh-hook preexec _prompt_preexec
+    add-zsh-hook precmd _prompt_precmd
+
+    PROMPT="%(?.%F{green}.%F{red})❯%f "
+}
+
+_prompt_setup
