0202b10aa54febad34745c1b9ca9f633e254b92c
Andrew Ho <andrewho@andrewho.co.uk>
2017-10-07 15:16:53 +0100

    Update ssh to use macOS Keychain
    This updates the ssh configuration to be in line with the changes documented
    in Apple's Technical Note TN2449:
    
    https://developer.apple.com/library/content/technotes/tn2449/_index.html#//apple_ref/doc/uid/DTS40017589

--- ssh/ssh-agent.zsh
+++ ssh/ssh-agent.zsh
@@ -1,19 +1,3 @@
-# If I am logged on to a console on a Mac then this is a local machine not 
-# server so setup ssh-agent and propagate across sessions via ~/.zshrc-ssh
-if [[ ! -n $SSH_TTY && `uname -s` == "Darwin" ]]; then
-    if [[ -a $HOME/.zshrc-ssh ]]; then
-        . $HOME/.zshrc-ssh
-    fi
-    if [[ $( ps -o comm= -p ${SSH_AGENT_PID} ) != "ssh-agent" ]]; then
-        local EXPORT_SSH
-        EXPORT_SSH=$( ssh-agent | head -n 2 )
-        echo $EXPORT_SSH > $HOME/.zshrc-ssh
-        eval $EXPORT_SSH
-        unset EXPORT_SSH
-        ssh-add $HOME/.ssh/id_rsa
-    fi
-fi
-
 # If this is my server (the only machine I forward an SSH agent to) then make
 # sure the socket is correctly setup with a link in ~/.ssh-agent-sock
 if [[ `hostname -s` == "sebastian" && -n $SSH_AUTH_SOCK ]]; then
