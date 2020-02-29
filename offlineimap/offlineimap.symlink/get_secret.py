354cef9efca7b5636884a5f9e0975eda705fa0e4
Andrew Ho <andrewho@andrewho.co.uk>
2020-02-29 13:45:04 +0000

    Overhaul of mutt / offlineimap configuratino

---
+++ offlineimap/offlineimap.symlink/get_secret.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os.path
+import subprocess
+
+
+def get_secret(account, item):
+    f = os.path.expanduser("~/.secrets.gpg")
+    try:
+        args = ["gpg", "--batch", "--quiet", "--decrypt", f]
+        stdout = subprocess.check_output(args)
+    except subprocess.CalledProcessError:
+        exit("Could not decrypt password file")
+
+    # Secrets are stored one per line as account:item:secret
+    secrets = [x.split(":") for x in stdout.splitlines()]
+    try:
+        secret = [x for x in secrets if x[0] == account and x[1] == item][0][2]
+    except IndexError:
+        exit("Could not find item")
+
+    return secret
