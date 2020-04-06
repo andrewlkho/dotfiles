6c8d090f6358aa20f3367d3d9dacb2bd43516b94
Andrew Ho <andrewho@andrewho.co.uk>
2020-04-06 14:14:46 +0100

    Move ssh configuration to stow-friendly path
    Also:
    1. Remove ssh-agent which was not actually being used
    2. Remove old functions ssh-proxy and ssh-tor
    3. Update iOS SSH key in authorized_keys

--- ssh/ssh-agent.zsh
+++
@@ -1,8 +0,0 @@
-# If this is my server (the only machine I forward an SSH agent to) then make
-# sure the socket is correctly setup with a link in ~/.ssh-agent-sock
-if [[ `hostname -s` == "sebastian" && -n $SSH_AUTH_SOCK ]]; then
-    if [[ $SSH_AUTH_SOCK == /tmp/* ]]; then
-        ln -sf $SSH_AUTH_SOCK $HOME/.ssh-agent-sock
-        export SSH_AUTH_SOCK=$HOME/.ssh-agent-sock
-    fi
-fi
