a83ef3f4c65a7db107810b472310465494178241
Andrew Ho <andrewho@andrewho.co.uk>
2020-04-09 03:18:57 +0100

    More fzf refactoring
    - No longer include .* for performance reasons
    - Minor adjustment of the find commands

--- zsh/.local/etc/zsh/lib/fzf.zsh
+++ zsh/.local/etc/zsh/lib/fzf.zsh
@@ -1,12 +1,18 @@
 if hash fzf &>/dev/null; then
     export FZF_DEFAULT_OPTS="--layout=reverse --border=sharp"
-    export FZF_CTRL_T_COMMAND="command find -L \"${HOME}\" -mindepth 1 \\( -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-        -o -path \"${HOME}/Library\" -prune \
+    export FZF_CTRL_T_COMMAND="command find -L \"${HOME}\" -mindepth 1 \
+        \\( -path '*/\\.*' \
+            -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \
+            -o -path \"${HOME}/Library\" \
+        \\) -prune \
         -o -type f -print \
         -o -type d -print \
         -o -type l -print 2> /dev/null"
-    export FZF_ALT_C_COMMAND="command find -L \"${HOME}\" -mindepth 1 \\( -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-        -o -path \"${HOME}/Library\" -prune \
+    export FZF_ALT_C_COMMAND="command find -L \"${HOME}\" -mindepth 1 \
+        \\( -path '*/\\.*' \
+            -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \
+            -o -path \"${HOME}/Library\" \
+        \\) -prune \
         -o -type d -print 2> /dev/null"
 
     source "${ZDOTDIR}/lib/fzf-keybindings"
