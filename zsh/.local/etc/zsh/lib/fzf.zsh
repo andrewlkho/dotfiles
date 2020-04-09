0462fb9aa6a7ac407ebb53ad051aedfb67baa47d
Andrew Ho <andrewho@andrewho.co.uk>
2020-04-09 03:06:05 +0100

    fzf Alt-C should exclude ~/Library

--- zsh/.local/etc/zsh/lib/fzf.zsh
+++ zsh/.local/etc/zsh/lib/fzf.zsh
@@ -5,6 +5,9 @@ if hash fzf &>/dev/null; then
         -o -type f -print \
         -o -type d -print \
         -o -type l -print 2> /dev/null"
+    export FZF_ALT_C_COMMAND="command find -L \"${HOME}\" -mindepth 1 \\( -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+        -o -path \"${HOME}/Library\" -prune \
+        -o -type d -print 2> /dev/null"
 
     source "${ZDOTDIR}/lib/fzf-keybindings"
 fi
