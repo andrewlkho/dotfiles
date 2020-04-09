ca8e1e5483c08a0d94a282e18579b2c195bf15cc
Andrew Ho <andrewho@andrewho.co.uk>
2020-04-09 02:30:51 +0100

    fzf: do not ignore hidden files

--- zsh/.local/etc/zsh/lib/fzf.zsh
+++ zsh/.local/etc/zsh/lib/fzf.zsh
@@ -1,6 +1,6 @@
 if hash fzf &>/dev/null; then
     export FZF_DEFAULT_OPTS="--layout=reverse --border=sharp"
-    export FZF_CTRL_T_COMMAND="command find -L \"${HOME}\" -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    export FZF_CTRL_T_COMMAND="command find -L \"${HOME}\" -mindepth 1 \\( -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
         -o -path \"${HOME}/Library\" -prune \
         -o -type f -print \
         -o -type d -print \
